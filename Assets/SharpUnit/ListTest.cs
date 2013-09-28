958e7f314f8595f004fade6d52d5b801f3f90510
Matthew Lenton <matthew.lenton@gmail.com>
2013-09-28 12:34:24 +1000

    initial commit

---
+++ Assets/SharpUnit/ListTest.cs
@@ -0,0 +1,102 @@
+﻿
+using SharpUnit;
+using SinglyLinkedListExample;
+
+namespace ListUnitTest 
+{ 
+	public class ListTest : TestCase 
+	{
+	
+		int[] test1array;
+		SinglyLinkedList<int> test1list;
+		
+		int[] test2array;
+		SinglyLinkedList<int> test2list;
+		
+		int[] test3array;
+		SinglyLinkedList<int> test3list;
+		
+		int[] test4array;
+		SinglyLinkedList<int> test4list;
+		
+		int[] test5array;
+		SinglyLinkedList<int> test5list;
+		
+	 
+		/** Setup test resources, called before each test. */
+	    public override void SetUp() 
+		{
+			test1array = new int[] {0,1,2,3,4,5,6,7,8,9};
+			test1list = new SinglyLinkedList<int>(test1array);
+
+			test2array = new int[] {-24, 42, 8, 0, 1, -5};
+			test2list = new SinglyLinkedList<int>(test2array);
+
+			test3array = new int[] {1,2,3,4,5};
+			test3list = new SinglyLinkedList<int>(test3array);
+
+			test4array = new int[] {};
+			test4list = new SinglyLinkedList<int>(test4array);
+
+			test5array = new int[] {1,2,3};
+			test5list = new SinglyLinkedList<int>(test5array);
+		}
+	
+	    /** Dispose of test resources, called after each test */
+	    public override void TearDown() 
+		{
+	        test1array = null;
+			test1list = null;
+	        test2array = null;
+			test2list = null;
+	        test3array = null;
+			test3list = null;
+	        test4array = null;
+			test4list = null;
+	        test5array = null;
+			test5list = null;
+	    }
+	
+	    /** Sample test that passes */
+	    [UnitTest]
+	    public void TestNormalOperation() 
+		{
+			/* 
+			 * check that it works properly in normal circumstances
+			 */
+			Assert.Equal( 5, test1list.GetNthLast(5).data );
+			Assert.Equal( 42, test2list.GetNthLast(5).data );
+			Assert.Equal( 1, test3list.GetNthLast(5).data );
+			Assert.Null( test4list.GetNthLast(5) );
+			Assert.Null( test5list.GetNthLast(5) );
+
+			int l = test1array.Length;
+			for (int i=0; i<l; i++)
+			{
+				Assert.Equal( i, test1list.GetNthLast(l-i).data );
+			}
+		}
+	
+	    /** Sample test that fails. */
+	    [UnitTest]
+	    public void TestRanges() 
+		{
+			/* 
+			 * there is no fifth-last element here, 
+			 * check that it won't cause a runtime error
+			 */
+			Assert.Equal( default(SingleLinkNode<int>), test2list.GetNthLast(10) );
+			
+			/* 
+			 * even though my exercise is only to find the fifth-last element, 
+			 * test negative integers passed to the function don't cause runtime error
+			 */
+			Assert.Equal( default(SingleLinkNode<int>), test2list.GetNthLast(-100) );
+
+		}
+
+	
+	}
+}
+
+
---
+++ Assets/SharpUnit/SinglyLinkedList.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace SinglyLinkedListExample { 
+	
+	public class SingleLinkNode<T>
+	{ 
+		public T data;
+		public SingleLinkNode<T> link;
+		public SingleLinkNode(T init) 
+		{ 
+			data = init;
+			link = null;
+		}
+	} 
+	
+	
+	public class SinglyLinkedList<T>
+	{ 
+		SingleLinkNode<T> head;
+		SingleLinkNode<T> current;
+	
+		public SinglyLinkedList(T[] a) 
+		{ 
+			for (int i=0; i<a.Length; i++)
+			{ 
+				Append(a[i]);
+			}
+		}
+	
+		public void Append(T o) 
+		{
+			if (head == null) 
+			{ 
+				head = new SingleLinkNode<T>(o);
+			}
+			else 
+			{
+				SingleLinkNode<T> newNode = new SingleLinkNode<T>(o);
+				
+				current = head; 
+				while (current.link != null)
+				{
+					current = current.link;
+				}
+				current.link = newNode;
+			}
+		}
+	
+		public SingleLinkNode<T> GetNthLast(int n) 
+		{
+			/*
+			 * the 0th last item doesn't strictly exist
+			 * since the 1st last item is the last
+			 * ...couldn't bring myself to preempt the meaning of 0 here and set n to 1
+			 */
+			if ( n <= 0 ) 
+			{ 
+				return default(SingleLinkNode<T>);
+			}
+			
+			SingleLinkNode<T> returned = head;
+			current = head;
+			
+			for (int i=1; i<n; i++)
+			{ 
+			
+				if (current != null) 
+				{ 
+					current = current.link;
+				} 
+				else 
+				{ 
+					return default(SingleLinkNode<T>);
+				}
+			}
+			
+			while ( (current.link != null) )  
+			{ 
+				current = current.link;
+				returned = returned.link;
+			} 
+			
+			if (returned == null) 
+			{				
+				return default(SingleLinkNode<T>);
+			} 
+			else
+			{ 
+				return returned;
+			}
+		}
+	}
+}
+
